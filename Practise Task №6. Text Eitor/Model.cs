8246f58ba0b02a32173fb4eafb41eb04054b0d4e
Giter228 <[200~isip_m.a.bordyan@mpt.ru>
2022-11-12 20:26:04 +0300

    some changed

---
+++ Practise Task №6. Text Eitor/Model.cs
@@ -0,0 +1,16 @@
+﻿namespace Practise_Task__6._Text_Eitor
+{
+    public class Model
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Description { get; set; }
+
+        public Model(string name, int age, string description)
+        {
+            Name = name;
+            Age = age;
+            Description = description;
+        }
+    }
+}
---
+++ Practise Task №6. Text Eitor/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Practise_Task__6._Text_Eitor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Run start = new Run();
+            
+            start.MainMenu();
+        }
+    }
+}
